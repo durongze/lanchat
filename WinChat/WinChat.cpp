dcb57217e729b9ac2b7d144eef457a58e3cf73f8
durongze@qq.com <durongze@qq.com>
2020-07-01 00:38:49 +0800

    WinChat 1.0

---
+++ WinChat/WinChat.cpp
@@ -0,0 +1,744 @@
+﻿// WinChat.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <winsock.h>
+#include <map>
+#include "WinChat.h"
+#include "DisplayManager.h"
+#include "DuplicationManager.h"
+#include "OutputManager.h"
+#include "ThreadManager.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量: 
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+HWND g_hIpAddr;
+HWND g_hPort;
+HWND g_hSendMsg;
+HWND g_hRecvMsg;
+HWND g_hConnBtn;
+HWND g_hSendBtn;
+HWND g_hCamera;
+
+HRESULT SystemTransitionsExpectedErrors[] = {
+	DXGI_ERROR_DEVICE_REMOVED,
+	DXGI_ERROR_ACCESS_LOST,
+	static_cast<HRESULT>(WAIT_ABANDONED),
+	S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIOutput1::DuplicateOutput due to a transition
+HRESULT CreateDuplicationExpectedErrors[] = {
+	DXGI_ERROR_DEVICE_REMOVED,
+	static_cast<HRESULT>(E_ACCESSDENIED),
+	DXGI_ERROR_UNSUPPORTED,
+	DXGI_ERROR_SESSION_DISCONNECTED,
+	S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIOutputDuplication methods due to a transition
+HRESULT FrameInfoExpectedErrors[] = {
+	DXGI_ERROR_DEVICE_REMOVED,
+	DXGI_ERROR_ACCESS_LOST,
+	S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIAdapter::EnumOutputs methods due to outputs becoming stale during a transition
+HRESULT EnumOutputsExpectedErrors[] = {
+	DXGI_ERROR_NOT_FOUND,
+	S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// 此代码模块中包含的函数的前向声明: 
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+#pragma comment(lib,"Ws2_32.lib ")
+typedef struct  {
+	long size;
+	char buf[1080 * 768];
+}TcpPackage;
+
+class TcpServer
+{
+	WSADATA wsaData;
+	struct sockaddr_in servAddr;
+	SOCKET serverSocket;
+	std::map<SOCKET, struct sockaddr_in> m_cli;
+public:
+	int Init()
+	{
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+		{
+			return 0;
+		}
+
+		/* 创建套接服务字 */
+		serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (serverSocket == INVALID_SOCKET)
+		{
+			return 0;
+		}
+
+		/* 设置IP地址 */
+		memset(&servAddr, 0, sizeof(servAddr));
+		servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		servAddr.sin_family = AF_INET;
+		servAddr.sin_port = htons(3000);
+		/* 绑定服务器套接字 */
+		if (::bind(serverSocket, (sockaddr*)&servAddr, sizeof(sockaddr)) == SOCKET_ERROR)
+		{
+			return 0;
+		}
+
+		/* 监听端口 */
+		if (listen(serverSocket, 20) == SOCKET_ERROR)
+		{
+			closesocket(serverSocket);
+			WSACleanup();
+			return 0;
+		}
+		return 0;
+	}
+
+	static int Accept(TcpServer *svr)
+	{
+		/* 阻塞方式等待accept */
+		struct sockaddr_in cliAddr;
+		int len = sizeof(cliAddr);
+		SOCKET clientSocket;
+		while (1) {
+			clientSocket = accept(svr->serverSocket, (sockaddr*)&cliAddr, &len);
+			if (clientSocket == INVALID_SOCKET)
+			{
+				return 0;
+			}
+			svr->m_cli.insert(std::make_pair(clientSocket, cliAddr));
+		}
+		return 0;
+	}
+
+	int Send(TcpPackage& img)
+	{
+		int ret;
+		std::map<SOCKET, struct sockaddr_in>::iterator iter;
+		for (iter = m_cli.begin(); iter != m_cli.end(); ++iter)
+		{
+			inet_ntoa(iter->second.sin_addr);
+			ret = send(iter->first, (char*)&img, sizeof(img), NULL);
+			if (ret > 0) {
+				return ret;
+			}
+		}
+		return 0;
+	}
+	int TransBitMap(const BITMAP& bm, BITMAPINFOHEADER& bi)
+	{
+		bi.biSize = { sizeof(bi) };
+		bi.biWidth = bm.bmWidth;
+		bi.biHeight = bm.bmHeight;
+		bi.biBitCount = bm.bmBitsPixel;
+		bi.biPlanes = bm.bmPlanes;
+		bi.biSizeImage = bm.bmWidthBytes * bm.bmHeight;
+
+		LONG lineSize = bi.biWidth * bi.biBitCount / 8;
+		BYTE* pLineData = new BYTE[lineSize];
+		BYTE* pStart;
+		BYTE* pEnd;
+		BYTE* pData = (BYTE*)bm.bmBits;
+		LONG lineStart = 0;
+		LONG lineEnd = bi.biHeight - 1;
+		while (lineStart < lineEnd)
+		{
+			pStart = pData + (lineStart * lineSize);
+			pEnd = pData + (lineEnd * lineSize);
+			// Swap the top with the bottom
+			memcpy(pLineData, pStart, lineSize);
+			memcpy(pStart, pEnd, lineSize);
+			memcpy(pEnd, pLineData, lineSize);
+			// Adjust the line index
+			lineStart++;
+			lineEnd--;
+		}
+		delete pLineData;
+	}
+
+	int Send(HBITMAP& bitmap)
+	{
+		BITMAP bm;
+		BITMAPINFOHEADER bi;
+		// HBITMAP bitmap = (HBITMAP)LoadImage(NULL, TEXT("test.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+		auto hcopy = (HBITMAP)CopyImage(bitmap, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+		GetObject(hcopy, sizeof(bm), &bm);
+		TransBitMap(bm, bi);
+		// send(sock, (BYTE*)bm.bmBits, sizeof(bi) + bi.biSizeImage, NULL)
+		DeleteObject(hcopy);
+	}
+	int Uninit()
+	{
+		std::map<SOCKET, struct sockaddr_in>::iterator iter;
+		for (iter = m_cli.begin(); iter != m_cli.end(); ++iter)
+		{
+			closesocket(iter->first);
+		}
+		WSACleanup();
+		return 0;
+	}
+};
+
+class TcpClient
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	SOCKET sock;
+public:
+	int Init()
+	{
+		if (WSAStartup(sockVersion, &wsaData) != 0) {
+			return 0;
+		}
+		return 0;
+	}
+
+	int Connect()
+	{
+		sockaddr_in sockAddr;
+		memset(&sockAddr, 0, sizeof(sockAddr));
+		sockAddr.sin_family = PF_INET;
+		sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		sockAddr.sin_port = htons(3000);
+		sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		while (connect(sock, (SOCKADDR*)& sockAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)
+		{
+			Sleep(1000);
+		}
+		return 0;
+	}
+
+	int Recv(TcpPackage& img)
+	{
+		int ret = recv(sock, (char*)&img, sizeof(img), 0);
+#if xxx
+		BYTE *buf = NULL;
+		BITMAPINFOHEADER* bi = (BITMAPINFOHEADER*)buf;
+		BYTE* bits = buf + sizeof(BITMAPINFOHEADER);
+		HBITMAP hbitmap = CreateBitmap(bi->biWidth, bi->biHeight, bi->biPlanes, bi->biBitCount, bits);
+		OpenClipboard(NULL);
+		EmptyClipboard();
+		SetClipboardData(CF_BITMAP, hbitmap);
+		CloseClipboard();
+#endif
+		return ret;
+	}
+
+	int Uninit()
+	{
+		closesocket(sock);
+		WSACleanup();
+		return 0;
+	}
+};
+//
+// Entry point for new duplication threads
+//
+DWORD WINAPI DDProc(_In_ void* Param)
+{
+	// Classes
+	DISPLAYMANAGER DispMgr;
+	DUPLICATIONMANAGER DuplMgr;
+
+	// D3D objects
+	ID3D11Texture2D* SharedSurf = nullptr;
+	IDXGIKeyedMutex* KeyMutex = nullptr;
+
+	// Data passed in from thread creation
+	THREAD_DATA* TData = reinterpret_cast<THREAD_DATA*>(Param);
+
+	// Get desktop
+	DUPL_RETURN Ret;
+	HDESK CurrentDesktop = nullptr;
+	CurrentDesktop = OpenInputDesktop(0, FALSE, GENERIC_ALL);
+	if (!CurrentDesktop)
+	{
+		// We do not have access to the desktop so request a retry
+		SetEvent(TData->ExpectedErrorEvent);
+		Ret = DUPL_RETURN_ERROR_EXPECTED;
+		goto Exit;
+	}
+
+	// Attach desktop to this thread
+	bool DesktopAttached = SetThreadDesktop(CurrentDesktop) != 0;
+	CloseDesktop(CurrentDesktop);
+	CurrentDesktop = nullptr;
+	if (!DesktopAttached)
+	{
+		// We do not have access to the desktop so request a retry
+		Ret = DUPL_RETURN_ERROR_EXPECTED;
+		goto Exit;
+	}
+
+	// New display manager
+	DispMgr.InitD3D(&TData->DxRes);
+
+	// Obtain handle to sync shared Surface
+	HRESULT hr = TData->DxRes.Device->OpenSharedResource(TData->TexSharedHandle, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&SharedSurf));
+	if (FAILED(hr))
+	{
+		Ret = ProcessFailure(TData->DxRes.Device, L"Opening shared texture failed", L"Error", hr, SystemTransitionsExpectedErrors);
+		goto Exit;
+	}
+
+	hr = SharedSurf->QueryInterface(__uuidof(IDXGIKeyedMutex), reinterpret_cast<void**>(&KeyMutex));
+	if (FAILED(hr))
+	{
+		Ret = ProcessFailure(nullptr, L"Failed to get keyed mutex interface in spawned thread", L"Error", hr);
+		goto Exit;
+	}
+
+	// Make duplication manager
+	Ret = DuplMgr.InitDupl(TData->DxRes.Device, TData->Output);
+	if (Ret != DUPL_RETURN_SUCCESS)
+	{
+		goto Exit;
+	}
+
+	// Get output description
+	DXGI_OUTPUT_DESC DesktopDesc;
+	RtlZeroMemory(&DesktopDesc, sizeof(DXGI_OUTPUT_DESC));
+	DuplMgr.GetOutputDesc(&DesktopDesc);
+
+	// Main duplication loop
+	bool WaitToProcessCurrentFrame = false;
+	FRAME_DATA CurrentData;
+
+	while ((WaitForSingleObjectEx(TData->TerminateThreadsEvent, 0, FALSE) == WAIT_TIMEOUT))
+	{
+		if (!WaitToProcessCurrentFrame)
+		{
+			// Get new frame from desktop duplication
+			bool TimeOut;
+			Ret = DuplMgr.GetFrame(&CurrentData, &TimeOut);
+			if (Ret != DUPL_RETURN_SUCCESS)
+			{
+				// An error occurred getting the next frame drop out of loop which
+				// will check if it was expected or not
+				break;
+			}
+
+			// Check for timeout
+			if (TimeOut)
+			{
+				// No new frame at the moment
+				continue;
+			}
+		}
+
+		// We have a new frame so try and process it
+		// Try to acquire keyed mutex in order to access shared surface
+		hr = KeyMutex->AcquireSync(0, 1000);
+		if (hr == static_cast<HRESULT>(WAIT_TIMEOUT))
+		{
+			// Can't use shared surface right now, try again later
+			WaitToProcessCurrentFrame = true;
+			continue;
+		}
+		else if (FAILED(hr))
+		{
+			// Generic unknown failure
+			Ret = ProcessFailure(TData->DxRes.Device, L"Unexpected error acquiring KeyMutex", L"Error", hr, SystemTransitionsExpectedErrors);
+			DuplMgr.DoneWithFrame();
+			break;
+		}
+
+		// We can now process the current frame
+		WaitToProcessCurrentFrame = false;
+
+		// Get mouse info
+		Ret = DuplMgr.GetMouse(TData->PtrInfo, &(CurrentData.FrameInfo), TData->OffsetX, TData->OffsetY);
+		if (Ret != DUPL_RETURN_SUCCESS)
+		{
+			DuplMgr.DoneWithFrame();
+			KeyMutex->ReleaseSync(1);
+			break;
+		}
+
+		// Process new frame
+		Ret = DispMgr.ProcessFrame(&CurrentData, SharedSurf, TData->OffsetX, TData->OffsetY, &DesktopDesc);
+		if (Ret != DUPL_RETURN_SUCCESS)
+		{
+			DuplMgr.DoneWithFrame();
+			KeyMutex->ReleaseSync(1);
+			break;
+		}
+
+		// Release acquired keyed mutex
+		hr = KeyMutex->ReleaseSync(1);
+		if (FAILED(hr))
+		{
+			Ret = ProcessFailure(TData->DxRes.Device, L"Unexpected error releasing the keyed mutex", L"Error", hr, SystemTransitionsExpectedErrors);
+			DuplMgr.DoneWithFrame();
+			break;
+		}
+
+		// Release frame back to desktop duplication
+		Ret = DuplMgr.DoneWithFrame();
+		if (Ret != DUPL_RETURN_SUCCESS)
+		{
+			break;
+		}
+	}
+
+Exit:
+	if (Ret != DUPL_RETURN_SUCCESS)
+	{
+		if (Ret == DUPL_RETURN_ERROR_EXPECTED)
+		{
+			// The system is in a transition state so request the duplication be restarted
+			SetEvent(TData->ExpectedErrorEvent);
+		}
+		else
+		{
+			// Unexpected error so exit the application
+			SetEvent(TData->UnexpectedErrorEvent);
+		}
+	}
+
+	if (SharedSurf)
+	{
+		SharedSurf->Release();
+		SharedSurf = nullptr;
+	}
+
+	if (KeyMutex)
+	{
+		KeyMutex->Release();
+		KeyMutex = nullptr;
+	}
+
+	return 0;
+}
+
+_Post_satisfies_(return != DUPL_RETURN_SUCCESS)
+DUPL_RETURN ProcessFailure(_In_opt_ ID3D11Device* Device, _In_ LPCWSTR Str, _In_ LPCWSTR Title, HRESULT hr, _In_opt_z_ HRESULT* ExpectedErrors)
+{
+	HRESULT TranslatedHr;
+
+	// On an error check if the DX device is lost
+	if (Device)
+	{
+		HRESULT DeviceRemovedReason = Device->GetDeviceRemovedReason();
+
+		switch (DeviceRemovedReason)
+		{
+		case DXGI_ERROR_DEVICE_REMOVED:
+		case DXGI_ERROR_DEVICE_RESET:
+		case static_cast<HRESULT>(E_OUTOFMEMORY) :
+		{
+			// Our device has been stopped due to an external event on the GPU so map them all to
+			// device removed and continue processing the condition
+			TranslatedHr = DXGI_ERROR_DEVICE_REMOVED;
+			break;
+		}
+
+		case S_OK:
+		{
+			// Device is not removed so use original error
+			TranslatedHr = hr;
+			break;
+		}
+
+		default:
+		{
+			// Device is removed but not a error we want to remap
+			TranslatedHr = DeviceRemovedReason;
+		}
+		}
+	}
+	else
+	{
+		TranslatedHr = hr;
+	}
+
+	// Check if this error was expected or not
+	if (ExpectedErrors)
+	{
+		HRESULT* CurrentResult = ExpectedErrors;
+
+		while (*CurrentResult != S_OK)
+		{
+			if (*(CurrentResult++) == TranslatedHr)
+			{
+				return DUPL_RETURN_ERROR_EXPECTED;
+			}
+		}
+	}
+
+	// Error was not expected so display the message box
+	DisplayMsg(Str, Title, TranslatedHr);
+
+	return DUPL_RETURN_ERROR_UNEXPECTED;
+}
+
+//
+// Displays a message
+//
+void DisplayMsg(_In_ LPCWSTR Str, _In_ LPCWSTR Title, HRESULT hr)
+{
+	if (SUCCEEDED(hr))
+	{
+		MessageBoxW(nullptr, Str, Title, MB_OK);
+		return;
+	}
+
+	const UINT StringLen = (UINT)(wcslen(Str) + sizeof(" with HRESULT 0x########."));
+	wchar_t* OutStr = new wchar_t[StringLen];
+	if (!OutStr)
+	{
+		return;
+	}
+
+	INT LenWritten = swprintf_s(OutStr, StringLen, L"%s with 0x%X.", Str, hr);
+	if (LenWritten != -1)
+	{
+		MessageBoxW(nullptr, OutStr, Title, MB_OK);
+	}
+
+	delete[] OutStr;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此放置代码。
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WINCHAT, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化: 
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINCHAT));
+
+    MSG msg;
+
+    // 主消息循环: 
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目的: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINCHAT));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINCHAT);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目的: 保存实例句柄并创建主窗口
+//
+//   注释: 
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+int HandleCmdMsg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId = LOWORD(wParam);
+	// 分析菜单选择: 
+	switch (wmId)
+	{
+	case IDM_ABOUT:
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+		break;
+	case IDM_EXIT:
+		DestroyWindow(hWnd);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+int HandlePaintMsg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	RECT rctA; // 定义一个RECT结构体，存储窗口的长宽高
+	HWND cam = g_hCamera;
+	::GetWindowRect(cam, &rctA); // 通过窗口句柄获得窗口的大小存储在rctA结构中
+	PAINTSTRUCT ps, psCamera;
+	HDC hdc = BeginPaint(hWnd, &ps);
+	HDC hdcCamera = BeginPaint(g_hCamera, &psCamera);
+	// TODO: 在此处添加使用 hdc 的任何绘图代码...
+	int nWidth = GetSystemMetrics(SM_CXSCREEN); // 得到屏幕的分辨率的x    
+	int nHeight = GetSystemMetrics(SM_CYSCREEN); // 得到屏幕分辨率的y    
+	HDC hdcDesk = GetDC(GetDesktopWindow()); // 得到屏幕的dc    
+	HDC hdcCopy = CreateCompatibleDC(hdcDesk); //  
+	HBITMAP hBitmap = CreateCompatibleBitmap(hdcDesk, rctA.right - rctA.left, rctA.bottom - rctA.top); // 得到位图    
+	HGDIOBJ hObj = SelectObject(hdcCopy, hBitmap); // 好像总得这么写。             
+	BitBlt(hdcCopy, 0, 0, nWidth, nHeight, hdcDesk, 0, 0, SRCCOPY);
+	BitBlt(hdcCamera, 0, 0, nWidth, nHeight, hdcCopy, 0, 0, SRCCOPY);
+	DeleteObject(hObj);
+	DeleteDC(hdcCopy);
+	DeleteDC(hdcDesk);
+	EndPaint(g_hCamera, &psCamera);
+	EndPaint(hWnd, &ps);
+	return 0;
+}
+
+int HandleCreateMsg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	CreateWindow(TEXT("static"), TEXT("IP地址:"),
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_CENTER,
+		30, 10, 60, 40,
+		hWnd, (HMENU)IDC_IP, hInst, NULL);
+	CreateWindow(TEXT("static"), TEXT("端口:"),
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_CENTER,
+		430, 10, 60, 40,
+		hWnd, (HMENU)IDC_PORT, hInst, NULL);
+	g_hIpAddr = CreateWindow(TEXT("edit"), NULL,
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+		100, 10, 300, 40,
+		hWnd, (HMENU)IDC_IP, hInst, NULL);
+	g_hPort = CreateWindow(TEXT("edit"), NULL,
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+		520, 10, 110, 40,
+		hWnd, (HMENU)IDC_PORT, hInst, NULL);
+	g_hRecvMsg = CreateWindow(TEXT("edit"), NULL,
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+		30, 70, 600, 200,
+		hWnd, (HMENU)IDC_RECV_MSG, hInst, NULL);
+	g_hSendMsg = CreateWindow(TEXT("edit"), NULL, 
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+		30, 300, 600, 80,
+		hWnd, (HMENU)IDC_SEND_MSG, hInst, NULL);
+
+	g_hConnBtn = CreateWindow(TEXT("button"), TEXT("连接"),
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER,
+		650, 10, 80, 40,
+		hWnd, (HMENU)IDC_CONNECT, hInst, NULL);
+	g_hSendBtn = CreateWindow(TEXT("button"), TEXT("发送"),
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER,
+		650, 330, 80, 40,
+		hWnd, (HMENU)IDC_SEND, hInst, NULL);
+	g_hCamera = CreateWindow(TEXT("static"), TEXT("视频:"),
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_CENTER,
+		710, 70, 320, 240,
+		hWnd, (HMENU)IDC_CAMERA, hInst, NULL);
+	return 0;
+}
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:    处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+		HandleCmdMsg(hWnd, message, wParam, lParam);
+        break;
+    case WM_PAINT:
+		HandlePaintMsg(hWnd, message, wParam, lParam);
+        break;
+	case WM_CREATE:
+		HandleCreateMsg(hWnd, message, wParam, lParam);
+		break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
